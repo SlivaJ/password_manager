3412fd52c55a86b7120bade41bf45cfcc3dc9aeb
John <jcsliva1999@gmail.com>
2021-11-17 18:13:19 -0500

    Fixed issue causing app to crash if the stored credentials page was opened more than once.

--- password-manager/src/js/stored_credentials.js
+++ password-manager/src/js/stored_credentials.js
@@ -1,68 +1,62 @@
-//Problem: Find some way to create a database of secure credentials(service,Username,Password)
-//solutions:
-//(Keytar): Failed: Uses built in os credential manager.
-//                  Easy enough to use but the windows credential manager sucks.
-//                  Cant hold multiple usernames and passwords from the same service
-//                  and multiple accounts like that are like 90% of the reason Im doing this...
-//(SQLite) : Research : Can be packaged in the app without need for added dependencies.
-// knex                 Something called SEE exists allowing sqlite to write and read encrypted data.
-//                      Not sure how to set this up so hopefully its not that hard 
-//(LokiJS & crypted file adapter): Research: currently installed in node dont forget to delete if I use sqlite
-//
-//
-//mongodb and mongoose to the rescue... probably
-//
-//
-//Current solution:(learning how to use json at the moment): using crypto.js and encrypting a json file that will contain all of the data
-//IV (initiation vector) added to the front of the encryption 
-//current version will have only one json file but might make the ability to add new sets later
-//
-//might also make a branch version of the app that uses a database but at this scale im not sure if its worth it. and I would have to have
-//one that could be encrypted as a whole and packaged with the application.
+//build connection to sqlite database
+//test CRUD functionality
+//encrypt data with crypto or built in thing SEE I think?
 
+//var db = new sqlite3.Database('src/app_data/test.sqlite3');   //this works starting from the 'password-manager' folder not from the js file it's called in.
 
-const sqlite3 = require('sqlite3');
+//following check function fixes the problem that caused the app to crash if the stored credential page was opened more than once.
+var isDatabaseSetup;
+function database_setup(){
+  if(isDatabaseSetup==false){
+    var sqlite3 = require('sqlite3').verbose();
+    db_setup = true;
+    console.log("setup");
+  }else if(isDatabaseSetup==true){
+    console.log("skipped dependency and connect")
+  }
+}
 
-var knex = require("knex")({
-    client: "sqlite3",
-    connection:{
-        filename: "../app_data/database.db"
-    },
-    useNullAsDefault: true
+connectDB();
+/* 
+function openDB(){
+  let db = new sqlite3.Database('src/app_data/database.db', (err) => {
+    if (err) {
+      console.error(err.message);
+    }
+    console.log('This was entirely too much effort');
+  });
+}
+  
+function querytesting(){
+  let sql = 'SELECT * FROM Info'
+
+  db.each(sql, function(err, row) {
+    console.log("User id: "+row.Username+" Password: "+ row.Password+" Tag: "+ row.Tag);
+  });
+}
+function closeConnection(){
+  db.close();
+}
+ */
+/* 
+//following code creates a database and adds values to it.
+//delete this code later because it was an example 
+db.serialize(function() {
+  db.run("CREATE TABLE user (id INT, dt TEXT)");
+
+  var stmt = db.prepare("INSERT INTO user VALUES (?,?)");
+  for (var i = 0; i < 10; i++) {
+  
+  var d = new Date();
+  var n = d.toLocaleTimeString();
+  stmt.run(i, n);
+  }
+  stmt.finalize();
+
+  db.each("SELECT id, dt FROM user", function(err, row) {
+      console.log("User id : "+row.id, row.dt);
+  });
 });
 
-
-knex.from('Info').select("*")
-    .then((rows) => {
-        for (row of rows) {
-            console.log(`${row['Username']} ${row['Password']} ${row['Tag']}`);
-        }
-    }).catch((err) => { console.log( err); throw err })
-    .finally(() => {
-        knex.destroy();
-    });
-
-
-
-
-
-//failed solution 1:
-/* const keytar = require('keytar');
-
-
-function keytarTest(){   
-   // keytar.setPassword('fakenetwork', 'AccountName', 'secret');
-    const secret = keytar.findPassword(search('gmail'));
-    secret.then((result) => {
-        if(result != null){
-            console.log("result: "+ result); // result will be 'secret'
-        }
-        else if(result == null){
-            console.log("fail");
-        }
-    });
-    /* keytar.getPassword('fakenetwork','john').then((result) => {
-        console.log("password: "+result) 
-
-}
+db.close();
  */
